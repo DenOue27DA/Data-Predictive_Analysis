88e6f677970404a1605366283faaa74ea3536f99
DenOue27DA <148264358+DenOue27DA@users.noreply.github.com>
2023-10-18 08:24:30 -0600

    Add files via upload

---
+++ Assignment 1.R
@@ -0,0 +1,8 @@
+#Assignment 1 – Working with Dataframes
+
+#Question 1
+dframe <- data.frame("person"=c("Stan","Francine","Steve","Roger","Hayley","Klaus"),"sex"=factor(c("M","F","M","M","F","M")),"funny"=factor(c("High","Med","Low","High","Med","Med")))
+
+#Question 2
+age <- c(41,41,15,21,60,1600)
+cbind(dframe,age)
