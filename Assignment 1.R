bc87ee5381d542e6e5e50e180f5c65735c549794
DenOue27DA <148264358+DenOue27DA@users.noreply.github.com>
2023-10-18 10:04:36 -0600

    Update Assignment 1.R

--- Assignment 1.R
+++ Assignment 1.R
@@ -4,5 +4,5 @@
 dframe <- data.frame("person"=c("Stan","Francine","Steve","Roger","Hayley","Klaus"),"sex"=factor(c("M","F","M","M","F","M")),"funny"=factor(c("High","Med","Low","High","Med","Med")))
 
 #Question 2
-age <- c(41,41,15,21,60,1600)
+age <- c(41,41,15,1600,21,60)
 cbind(dframe,age)
